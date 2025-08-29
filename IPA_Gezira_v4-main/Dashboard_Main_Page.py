394f18fe67b09cb097e4d607fcda5a5ac1c434e3
HanaAltom <hmohamme@uni-bonn.de>
2025-08-29 17:11:58 +0200

    Update Dashboard_Main_Page.py

--- IPA_Gezira_v4-main/Dashboard_Main_Page.py
+++ IPA_Gezira_v4-main/Dashboard_Main_Page.py
@@ -80,8 +80,16 @@ with col2:
 	st.write ('')
 	st.write('')
 	st.write('')
-		
-	st.image("https://github.com/HanaAltom/Gezira_IPA_Team/blob/main/data/Gezira_Scheme.png", caption="Gezira Scheme Divisions", width=400)
+	Gezira_scheme_image = "https://github.com/HanaAltom/Gezira_IPA_Team/blob/main/data/Gezira_Scheme.png"
+	response = requests.get(Gezira_scheme_image)
+
+	if response.status_code == 200:
+    img = Image.open(BytesIO(response.content))
+    st.image(img, caption="Image from URL", use_container_width=True)
+	else:
+    st.error("Could not fetch image")	
+	st.image(Gezira_scheme_image, caption="Gezira Scheme Divisions", width=400)
+
 
 
 
